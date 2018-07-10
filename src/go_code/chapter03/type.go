94dbc8c948d3cbbe0900ea10b0a8cc8aebb29a17
YoungNovice <2306915539@qq.com>
2018-07-10 09:41:35 +0800

    go h update

---
+++ src/go_code/chapter03/type.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+	"fmt"
+	"go_code/chapter03/model"
+)
+
+// 值类型 int系列 float系列 bool string 数组 strcut
+
+// 引用类型 指针 slice map chan interface
+
+ func main () {
+
+	fmt.Println("model中的默认值 N = ", model.N)
+ }
